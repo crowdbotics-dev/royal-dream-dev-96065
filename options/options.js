0dc79f2261dd546c47dd1a6d575664176522ee96
Crowdbotics <engineering@crowdbotics.com>
2023-06-29 17:07:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-dream-dev-96065",
+  url: "https://royal-dream-dev-96065.botics.co",
+  api: "https://royal-dream-dev-96065.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
